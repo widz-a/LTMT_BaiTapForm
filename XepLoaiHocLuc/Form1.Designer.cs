50c6b875c2033a9ffd3c7ad38db9902eb8750b84
Wida <itswithermc@gmail.com>
2025-10-28 14:18:28 +0700

    Added XepLoaiHocLuc

---
+++ XepLoaiHocLuc/Form1.Designer.cs
@@ -0,0 +1,422 @@
+﻿namespace XepLoaiHocLuc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            tableLayoutPanel2 = new TableLayoutPanel();
+            btn_exit = new Button();
+            groupBox1 = new GroupBox();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            input_s5 = new TextBox();
+            label7 = new Label();
+            input_s4 = new TextBox();
+            label6 = new Label();
+            input_s3 = new TextBox();
+            label5 = new Label();
+            input_s2 = new TextBox();
+            label4 = new Label();
+            input_s1 = new TextBox();
+            label3 = new Label();
+            tableLayoutPanel5 = new TableLayoutPanel();
+            output_tbc = new TextBox();
+            btn_reset = new Button();
+            btn_exec = new Button();
+            label8 = new Label();
+            output_info = new TextBox();
+            input_name = new TextBox();
+            label2 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            panel1 = new Panel();
+            tableLayoutPanel4 = new TableLayoutPanel();
+            label1 = new Label();
+            tableLayoutPanel2.SuspendLayout();
+            groupBox1.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
+            tableLayoutPanel5.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
+            tableLayoutPanel4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 129F));
+            tableLayoutPanel2.Controls.Add(btn_exit, 1, 1);
+            tableLayoutPanel2.Controls.Add(groupBox1, 0, 0);
+            tableLayoutPanel2.Controls.Add(tableLayoutPanel5, 1, 0);
+            tableLayoutPanel2.Controls.Add(output_info, 0, 1);
+            tableLayoutPanel2.Dock = DockStyle.Top;
+            tableLayoutPanel2.Location = new Point(0, 88);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 2;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 68.77193F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 31.22807F));
+            tableLayoutPanel2.Size = new Size(423, 285);
+            tableLayoutPanel2.TabIndex = 2;
+            // 
+            // btn_exit
+            // 
+            btn_exit.Dock = DockStyle.Fill;
+            btn_exit.Location = new Point(297, 199);
+            btn_exit.Name = "btn_exit";
+            btn_exit.Size = new Size(123, 83);
+            btn_exit.TabIndex = 3;
+            btn_exit.Text = "Thoát";
+            btn_exit.UseVisualStyleBackColor = true;
+            btn_exit.Click += btn_exit_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(tableLayoutPanel3);
+            groupBox1.Dock = DockStyle.Top;
+            groupBox1.Location = new Point(3, 3);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(288, 190);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Nhập Trung Bình Môn";
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.ColumnCount = 2;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 82.93515F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 17.064846F));
+            tableLayoutPanel3.Controls.Add(input_s5, 1, 4);
+            tableLayoutPanel3.Controls.Add(label7, 0, 4);
+            tableLayoutPanel3.Controls.Add(input_s4, 1, 3);
+            tableLayoutPanel3.Controls.Add(label6, 0, 3);
+            tableLayoutPanel3.Controls.Add(input_s3, 1, 2);
+            tableLayoutPanel3.Controls.Add(label5, 0, 2);
+            tableLayoutPanel3.Controls.Add(input_s2, 1, 1);
+            tableLayoutPanel3.Controls.Add(label4, 0, 1);
+            tableLayoutPanel3.Controls.Add(input_s1, 1, 0);
+            tableLayoutPanel3.Controls.Add(label3, 0, 0);
+            tableLayoutPanel3.Dock = DockStyle.Fill;
+            tableLayoutPanel3.Location = new Point(3, 23);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 5;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel3.Size = new Size(282, 164);
+            tableLayoutPanel3.TabIndex = 0;
+            // 
+            // input_s5
+            // 
+            input_s5.Dock = DockStyle.Fill;
+            input_s5.Location = new Point(236, 131);
+            input_s5.Name = "input_s5";
+            input_s5.Size = new Size(43, 27);
+            input_s5.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Dock = DockStyle.Fill;
+            label7.Location = new Point(3, 128);
+            label7.Name = "label7";
+            label7.Size = new Size(227, 36);
+            label7.TabIndex = 10;
+            label7.Text = "An Toàn Mạng";
+            label7.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // input_s4
+            // 
+            input_s4.Dock = DockStyle.Fill;
+            input_s4.Location = new Point(236, 99);
+            input_s4.Name = "input_s4";
+            input_s4.Size = new Size(43, 27);
+            input_s4.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Dock = DockStyle.Fill;
+            label6.Location = new Point(3, 96);
+            label6.Name = "label6";
+            label6.Size = new Size(227, 32);
+            label6.TabIndex = 8;
+            label6.Text = "Quản Trị Mạng 2";
+            label6.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // input_s3
+            // 
+            input_s3.Dock = DockStyle.Fill;
+            input_s3.Location = new Point(236, 67);
+            input_s3.Name = "input_s3";
+            input_s3.Size = new Size(43, 27);
+            input_s3.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Dock = DockStyle.Fill;
+            label5.Location = new Point(3, 64);
+            label5.Name = "label5";
+            label5.Size = new Size(227, 32);
+            label5.TabIndex = 6;
+            label5.Text = "Quản trị CSDL Khách/Chủ";
+            label5.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // input_s2
+            // 
+            input_s2.Dock = DockStyle.Fill;
+            input_s2.Location = new Point(236, 35);
+            input_s2.Name = "input_s2";
+            input_s2.Size = new Size(43, 27);
+            input_s2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Dock = DockStyle.Fill;
+            label4.Location = new Point(3, 32);
+            label4.Name = "label4";
+            label4.Size = new Size(227, 32);
+            label4.TabIndex = 4;
+            label4.Text = "Lập Trình Trực Quan C#";
+            label4.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // input_s1
+            // 
+            input_s1.Dock = DockStyle.Fill;
+            input_s1.Location = new Point(236, 3);
+            input_s1.Name = "input_s1";
+            input_s1.Size = new Size(43, 27);
+            input_s1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = DockStyle.Fill;
+            label3.Location = new Point(3, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(227, 32);
+            label3.TabIndex = 0;
+            label3.Text = "ISA Server 2006";
+            label3.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // tableLayoutPanel5
+            // 
+            tableLayoutPanel5.ColumnCount = 1;
+            tableLayoutPanel5.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel5.Controls.Add(output_tbc, 0, 3);
+            tableLayoutPanel5.Controls.Add(btn_reset, 0, 1);
+            tableLayoutPanel5.Controls.Add(btn_exec, 0, 0);
+            tableLayoutPanel5.Controls.Add(label8, 0, 2);
+            tableLayoutPanel5.Dock = DockStyle.Fill;
+            tableLayoutPanel5.Location = new Point(297, 3);
+            tableLayoutPanel5.Name = "tableLayoutPanel5";
+            tableLayoutPanel5.RowCount = 4;
+            tableLayoutPanel5.RowStyles.Add(new RowStyle(SizeType.Percent, 40F));
+            tableLayoutPanel5.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel5.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel5.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel5.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel5.Size = new Size(123, 190);
+            tableLayoutPanel5.TabIndex = 1;
+            // 
+            // output_tbc
+            // 
+            output_tbc.Dock = DockStyle.Fill;
+            output_tbc.ForeColor = Color.Red;
+            output_tbc.Location = new Point(3, 155);
+            output_tbc.Name = "output_tbc";
+            output_tbc.Size = new Size(117, 27);
+            output_tbc.TabIndex = 11;
+            output_tbc.TextAlign = HorizontalAlignment.Center;
+            // 
+            // btn_reset
+            // 
+            btn_reset.Dock = DockStyle.Fill;
+            btn_reset.Location = new Point(3, 79);
+            btn_reset.Name = "btn_reset";
+            btn_reset.Size = new Size(117, 32);
+            btn_reset.TabIndex = 1;
+            btn_reset.Text = "Reset";
+            btn_reset.UseVisualStyleBackColor = true;
+            // 
+            // btn_exec
+            // 
+            btn_exec.Dock = DockStyle.Fill;
+            btn_exec.Location = new Point(3, 3);
+            btn_exec.Name = "btn_exec";
+            btn_exec.Size = new Size(117, 70);
+            btn_exec.TabIndex = 0;
+            btn_exec.Text = "Nhập";
+            btn_exec.UseVisualStyleBackColor = true;
+            btn_exec.Click += btn_exec_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Dock = DockStyle.Fill;
+            label8.ImageAlign = ContentAlignment.BottomCenter;
+            label8.Location = new Point(3, 114);
+            label8.Name = "label8";
+            label8.Size = new Size(117, 38);
+            label8.TabIndex = 12;
+            label8.Text = "Trung bình cộng";
+            label8.TextAlign = ContentAlignment.BottomCenter;
+            // 
+            // output_info
+            // 
+            output_info.Dock = DockStyle.Top;
+            output_info.ForeColor = SystemColors.HotTrack;
+            output_info.Location = new Point(3, 199);
+            output_info.Multiline = true;
+            output_info.Name = "output_info";
+            output_info.Size = new Size(288, 83);
+            output_info.TabIndex = 2;
+            // 
+            // input_name
+            // 
+            input_name.Dock = DockStyle.Fill;
+            input_name.Location = new Point(153, 3);
+            input_name.Name = "input_name";
+            input_name.Size = new Size(267, 27);
+            input_name.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = DockStyle.Left;
+            label2.Location = new Point(3, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(134, 35);
+            label2.TabIndex = 1;
+            label2.Text = "Nhập tên sinh viên:";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 150F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(label2, 0, 0);
+            tableLayoutPanel1.Controls.Add(input_name, 1, 0);
+            tableLayoutPanel1.Dock = DockStyle.Top;
+            tableLayoutPanel1.Location = new Point(0, 53);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(423, 35);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(tableLayoutPanel4);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(423, 53);
+            panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel4
+            // 
+            tableLayoutPanel4.ColumnCount = 1;
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel4.Controls.Add(label1, 0, 0);
+            tableLayoutPanel4.Dock = DockStyle.Fill;
+            tableLayoutPanel4.Location = new Point(0, 0);
+            tableLayoutPanel4.Name = "tableLayoutPanel4";
+            tableLayoutPanel4.RowCount = 1;
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel4.Size = new Size(423, 53);
+            tableLayoutPanel4.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Fill;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Red;
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(417, 53);
+            label1.TabIndex = 0;
+            label1.Text = "Xếp Loại Học Lực";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(423, 373);
+            Controls.Add(tableLayoutPanel2);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Xếp Loại Học Lực";
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            tableLayoutPanel3.ResumeLayout(false);
+            tableLayoutPanel3.PerformLayout();
+            tableLayoutPanel5.ResumeLayout(false);
+            tableLayoutPanel5.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            panel1.ResumeLayout(false);
+            tableLayoutPanel4.ResumeLayout(false);
+            tableLayoutPanel4.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private TableLayoutPanel tableLayoutPanel2;
+        private GroupBox groupBox1;
+        private TableLayoutPanel tableLayoutPanel3;
+        private Label label3;
+        private TextBox input_s5;
+        private Label label7;
+        private TextBox input_s4;
+        private Label label6;
+        private TextBox input_s3;
+        private Label label5;
+        private TextBox input_s2;
+        private Label label4;
+        private TextBox input_s1;
+        private TextBox input_name;
+        private Label label2;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Panel panel1;
+        private TableLayoutPanel tableLayoutPanel4;
+        private Label label1;
+        private TableLayoutPanel tableLayoutPanel5;
+        private Button btn_exec;
+        private TextBox output_tbc;
+        private Button btn_reset;
+        private Label label8;
+        private TextBox output_info;
+        private Button btn_exit;
+    }
+}
---
+++ XepLoaiHocLuc/Form1.cs
@@ -0,0 +1,53 @@
+﻿namespace XepLoaiHocLuc
+{
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void btn_exec_Click(object sender, EventArgs e) {
+            string name = input_name.Text.Trim();
+
+            TextBox[] textboxes = { input_s1, input_s2, input_s3, input_s4, input_s5 };
+            List<double> grades = new List<double>();
+            
+            foreach (var item in textboxes) {
+                double n;
+                if (double.TryParse(item.Text, out n)) {
+                    grades.Add(n);
+                } else {
+                    MessageBox.Show("Điểm không hợp lệ: " + item.Text, "Lỗi");
+                    break;
+                }
+            }
+
+            // Check lại xem có đủ điểm ko (do lỗi sẽ ko thêm vô grades)!!!
+            if (grades.Count != textboxes.Length) {
+                return;
+            }
+
+            //Tính TBC
+            double sum = 0;
+            foreach (var item in grades) {
+                sum += item;
+            }
+            double average = sum / grades.Count;
+            output_tbc.Text = average.ToString();
+
+            string xepLoai;
+            if (average > 10) xepLoai = "Súc vật";
+            else if (average >= 9) xepLoai = "Xuất sắc";
+            else if (average >= 8) xepLoai = "Giỏi";
+            else if (average >= 7) xepLoai = "Khá";
+            else if (average >= 5) xepLoai = "Trung bình";
+            else if (average >= 4) xepLoai = "Yếu";
+            else xepLoai = "Yếu";
+
+            output_info.Text = $"{name}\r\nXếp loại: {xepLoai}";
+        }
+    }
+}
